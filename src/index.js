e05d5f377424f3b3f0546fcd702f74c9acaf9284
David Hazinski <david.hazinski@transloc.com>
2017-02-16 13:12:55 -0500

    first commit

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { render } from 'react-snapshot';
+import App from './App';
+import './index.css';
+
+ReactDOM.render(
+  <App />,
+  document.getElementById('root')
+);
