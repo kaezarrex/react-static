562a4f425b0daf0c69c836b074ec16e48c7d8b11
David Hazinski <dhazinski@gmail.com>
2017-02-19 14:56:04 -0500

    Add navigation

---
+++ src/About.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const About = () => (
+  <p>
+    This is the About page.
+  </p>
+);
+
+export default About;
--- src/App.js
+++ src/App.js
@@ -1,21 +1,50 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
+import {BrowserRouter as Router, Route, Switch, Link} from 'react-router-dom';
 import Helmet from 'react-helmet';
+
+import Home from './Home';
+import About from './About';
+import NoMatch from './NoMatch';
 import logo from './logo.svg';
 import './App.css';
+
+const routes = [
+  {
+    title: 'Home',
+    path: '/',
+    component: Home,
+    exact: true,
+  },
+  {
+    title: 'About',
+    path: '/about',
+    component: About,
+  },
+];
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <Helmet title="Travis is great" />
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+      <Router>
+        <div className="App">
+          <Helmet title="Travis is great" />
+          <div className="App-header">
+            <img src={logo} className="App-logo" alt="logo" />
+            <h2>Welcome to React</h2>
+          </div>
+
+          <div>
+            <h1>Navigation</h1>
+            {routes.map((route, i) => <Link key={i} to={route.path}>{route.title}</Link>)}
+          </div>
+
+          <Switch>
+            {routes.map((route, i) => <Route key={i} {...route} />)}
+            <Route component={NoMatch} />
+          </Switch>
+
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      </Router>
     );
   }
 }
